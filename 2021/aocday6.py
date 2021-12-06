278c547b1612e5e2a2aa8c82ab5e7a5d7c1054c2
AntiAki <akimatti.antikainen@gmail.com>
2021-12-06 15:31:43 +0200

    Clean up and add iterative solution to day 6 part 2
    Shouldn't use recursion with python, unless recursion depth is small

--- 2021/aocday6.py
+++ 2021/aocday6.py
@@ -14,23 +14,7 @@ def initialize_fish():
     return initial_lanternfish, initial_counts
 
 
-# recursively grow list of all fish
-def simulate_lanternfish(lanternfish_list, days):
-    new_fish = []
-
-    for fish in lanternfish_list:
-        if fish == 0:
-            new_fish.extend([int(CONFIG["cycle"]), int(CONFIG["birth"])])
-        else:
-            new_fish.append(fish - 1)
-    if days != 1:
-        return simulate_lanternfish(new_fish, days - 1)
-    else:
-        return new_fish
-
-
-# Instead of growing a huge list, recursively manage only counts
-def dynamic_simulate_lanternfish(counts, days):
+def count_fish(counts):
     new_counts = defaultdict(int)
     for i in sorted([int(key) for key in counts.keys()]):
         dict_key = str(i)
@@ -39,14 +23,41 @@ def dynamic_simulate_lanternfish(counts, days):
             new_counts[CONFIG["cycle"]] = counts[dict_key]
         else:
             new_counts[str(i - 1)] += counts[dict_key]
+    return new_counts
 
-    if days != 1:
-        return dynamic_simulate_lanternfish(new_counts, days - 1)
+
+# recursively grow list of all fish
+def recursive_simulate_lanternfish(lanternfish_list, days):
+    if days != 0:
+        new_fish = []
+        for fish in lanternfish_list:
+            if fish == 0:
+                new_fish.extend([int(CONFIG["cycle"]), int(CONFIG["birth"])])
+            else:
+                new_fish.append(fish - 1)
+        return recursive_simulate_lanternfish(new_fish, days - 1)
     else:
-        return sum(new_counts.values())
+        return len(lanternfish_list)
+
+
+# Instead of growing a huge list, recursively manage only counts
+def dynamic_recursive_simulate_lanternfish(counts, days):
+    if days != 0:
+        new_counts = count_fish(counts)
+        return dynamic_recursive_simulate_lanternfish(new_counts, days - 1)
+    else:
+        return sum(counts.values())
+
+
+def dynamic_simulate_lantern_fish_iterative(counts, days):
+    for i in range(0, days):
+        new_counts = count_fish(counts)
+        counts = new_counts
+    return sum(counts.values())
 
 
 if __name__ == '__main__':
     init_fish, init_count = initialize_fish()
-    print(len(simulate_lanternfish(init_fish, 80)))  # part1 solution
-    print(dynamic_simulate_lanternfish(init_count, 256))  # part2 solution
+    print(recursive_simulate_lanternfish(init_fish, 80))  # part1 solution
+    print(dynamic_recursive_simulate_lanternfish(init_count, 256))  # part2 solution, limited to max recursion depth
+    print(dynamic_simulate_lantern_fish_iterative(init_count, 256))  # part2 iterative solution, no limits
